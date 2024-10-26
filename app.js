835bd2dcca091fbfc62e8d4a693c1c3814c25e8c
Md Mohosin Ali Shah <mohosin2126@gmail.com>
2024-10-27 01:58:44 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase
 import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 let confirmationResult;
 const firebaseConfig = {
-    apiKey: "AIzaSyAQ7k2jTL7CIV0i8fjKfxcddJShslQtYPo",
-    authDomain: "cpalance-c48a7.firebaseapp.com",
-    projectId: "cpalance-c48a7",
-    storageBucket: "cpalance-c48a7.appspot.com",
-    messagingSenderId: "587490960161",
-    appId: "1:587490960161:web:c58158bc24dbc2fd0b1460",
-    measurementId: "G-38VYMSTK0N"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
